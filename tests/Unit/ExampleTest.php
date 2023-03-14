abefbac7b76f16b8dde8851c089ba8b7dc96453b
Brian Faust <hello@basecode.sh>
2023-03-14 07:12:02 +0200

    chore: organise tests directory

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+it('should be true', function () {
+    expect(true)->toBeTrue();
+});
+
+it('should be false', function () {
+    expect(false)->toBeFalse();
+});
