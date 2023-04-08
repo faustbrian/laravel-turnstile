dbcea344e254df3d5dd07a8c93cc54bfcf5a12d4
Brian Faust <hello@basecode.sh>
2023-04-08 10:49:58 +0300

    chore: update jetpack

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,25 +4,15 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Orchestra\Testbench\TestCase as Orchestra;
-use PreemStudio\Turnstile\ServiceProvider;
-use Spatie\LaravelData\LaravelDataServiceProvider;
+use PreemStudio\Jetpack\TestBench\AbstractPackageTestCase;
 
 /**
  * @internal
  */
-abstract class TestCase extends Orchestra
+abstract class TestCase extends AbstractPackageTestCase
 {
-    public function getEnvironmentSetUp($app): void
+    protected function getServiceProviderClass(): string
     {
-        config()->set('database.default', 'testing');
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            LaravelDataServiceProvider::class,
-            ServiceProvider::class,
-        ];
+        return \PreemStudio\Turnstile\ServiceProvider::class;
     }
 }
